e9c1a0905768d5ea31415f1af66c6c0c4adb425f
MesssyAdam <messsy.adam@gmail.com>
2018-03-21 13:38:43 -0400

    modified kgreeting.rb

--- lib/toomarco/kgreeting.rb
+++ lib/toomarco/kgreeting.rb
@@ -3,7 +3,6 @@ choice = gets.to_s
 if choice
   puts "Hello Marc!" if choice == "y" or "Y" or "yes" or "Yes"
   puts "No problem! Goodbye!"  if choice == "n" or "N" or "no" or "No"
-elsif choice !=
   puts <<EMO
   Incorrect input, would you like to translate 
   this statement in english, yes or no or would
